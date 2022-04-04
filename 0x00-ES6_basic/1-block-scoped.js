83649053c351dcb789ab97ed85c97a945fa00263
Daniel-bee <daniel123_86@live.com>
2022-04-04 19:10:59 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
-/* block scope */
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task;
+  let task2;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }
