b60f0497bc3cfbaf26ed127dd5a5057d5356ed34
Daniel-bee <daniel123_86@live.com>
2022-04-04 17:24:03 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task;
+  var task2;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    var task = true;
+    var task2 = false;
   }
-
+  task = false;
+  task2 = true;
   return [task, task2];
 }
